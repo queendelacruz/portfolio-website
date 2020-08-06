adff26ad8035ebe8feb2b290a198b6dc5cbae6d3
queendelacruz <queendelacruz09@gmail.com>
2020-08-05 19:53:55 -0700

    Set up the first web page  - Added a profile pic - Create buttons to navigate to other pages and other links

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React from 'react';
-import NavigationBar from './NavigationBar.js'
+import {Image, Button, Container, Col, Row} from 'react-bootstrap';
+import { AiFillLinkedin, AiFillMail, AiFillGithub} from 'react-icons/ai';
+import {IconContext } from 'react-icons';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
-      <NavigationBar />
-      
-      <div className="App-body d-flex">
-        <p>Portfolio</p>
+      {/* <Image roundedCircle style={{ width: '10rem' }} src="images/profile.jpg" class="ml-auto rounded-circle"/> */}
+      <div className="App-body ">
+        <h1>Hi! I'm Queen Dela Cruz</h1>
+        <p>Recently graduated with a Bachelor of Science in Software Systems from Simon Fraser University.</p>
+        <Container>
+          <Row>
+            <Col>
+              <IconContext.Provider value={{className: "rounded-circle ", size: 50, color:"#1f1e2f"}}>
+                <a href="https://www.linkedin.com/in/queen-dela-cruz-867331ab/"> <AiFillLinkedin /></a>
+                <a href="mailto:queenie.dcrz@gmail.com"><AiFillMail /></a>
+                <a href="https://github.com/queendelacruz"><AiFillGithub /></a>
+              </IconContext.Provider>
+            </Col>
+            <Col>
+              <div  className="float-right">
+                <Button >Projects</Button>{' '}
+                <Button >Experience</Button>{' '}
+                <Button >Skills</Button>
+              </div>
+            </Col>
+          </Row>
+        
+        </Container>
+        
       </div>
+        
     </div>
   );
 }
